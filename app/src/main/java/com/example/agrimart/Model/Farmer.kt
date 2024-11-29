25e76b4aed182c9e22fcd4e0b8f7ee349cea7e39
Mominul Islam <mominulislam23295@gmail.com>
2024-11-29 22:01:35 +0600

    First Commit

---
+++ app/src/main/java/com/example/agrimart/Model/Farmer.kt
@@ -0,0 +1,14 @@
+package com.example.agrimart.Model
+
+class Farmer(
+    var id:String?=null,
+    var name:String?=null,
+    var phone:String?=null,
+    var pic:String?=null,
+    var password:String?=null,
+    var balance:String?=null,
+    var division: String? = null,
+    var district: String? = null,
+    var thana: String? = null,
+) {
+}
---
+++ app/src/main/java/com/example/agrimart/Model/Trader.kt
@@ -0,0 +1,14 @@
+package com.example.agrimart.Model
+
+class Trader(
+    var id:String?=null,
+    var name:String?=null,
+    var phone:String?=null,
+    var pic:String?=null,
+    var password:String?=null,
+    var balance:String?=null,
+    var division: String? = null,
+    var district: String? = null,
+    var thana: String? = null,
+    ) {
+}
